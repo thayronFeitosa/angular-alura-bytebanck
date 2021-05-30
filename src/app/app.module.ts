a8aab0c91d416668b67f2b9ce7da05d960ed974d
thayron <thayronf3@gmail.com>
2021-05-30 09:31:51 -0300

    Foi iniciado o projeto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NovaTrasnferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTrasnferenciaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'nova-tranferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+
+})
+
+export class NovaTrasnferenciaComponent{
+
+}
